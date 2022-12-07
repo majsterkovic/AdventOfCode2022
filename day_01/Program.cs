b6ef68c9d1683d527ec8688f816a70ea9c8b9f03
majsterkovic <mariusz8h@o2.pl>
2022-12-07 03:19:25 +0100

    Day 1: Calorie Counting

---
+++ day_01/Program.cs
@@ -0,0 +1,27 @@
+﻿// Day 1: Calorie Counting
+
+
+var lines = File.ReadAllLines(@"input");
+lines.Append("");
+var sum = 0;
+
+var listOfCaloriesSum = new List<int>();
+
+foreach (var line in lines)
+    if (line != "")
+    {
+        var number = int.Parse(line);
+        sum += number;
+    }
+    else
+    {
+        listOfCaloriesSum.Add(sum);
+        sum = 0;
+    }
+
+var max = listOfCaloriesSum.Max();
+var index = listOfCaloriesSum.IndexOf(max);
+Console.WriteLine("Elf number {0} carries the most calories: {1}.", index + 1, max);
+
+var maxThreeSum = listOfCaloriesSum.OrderByDescending(x => x).Take(3).Sum();
+Console.WriteLine("Top three Elves are carrying in total {0} calories.", maxThreeSum);
